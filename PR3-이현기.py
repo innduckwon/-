aa3bbbb5d0e5dbbf5f923ee09e3c215d4df48d76
innduckwon <innduckwon@gmail.com>
2022-04-05 20:40:55 +0900

    실습과제

---
+++ PR3-이현기.py
@@ -0,0 +1,392 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+a=[1,2,3]
+
+
+# In[ ]:
+
+
+b=(1,2,3)
+
+
+# In[ ]:
+
+
+a[0]=2
+
+
+# In[ ]:
+
+
+b[0]=1
+
+
+# In[92]:
+
+
+print(b[1:])
+
+
+# In[5]:
+
+
+dic={'a':10,'b':20,'c':30}
+dic
+
+
+# In[6]:
+
+
+print(dic['a'])
+
+
+# In[7]:
+
+
+dic['d']=40
+dic
+
+
+# In[9]:
+
+
+dic['a']=30
+dic
+
+
+# In[11]:
+
+
+dic['e']=[1,2]
+dic
+
+
+# In[12]:
+
+
+k={1[1,2]:'hi'}
+
+
+# In[13]:
+
+
+k={(1,2):1}
+
+
+# In[23]:
+
+
+a=['yes','no','no','no','no','yes','yes']
+d={'yes':1,'no':0}
+new_a=[d[key] for key in a]
+print(new_a)
+
+
+# In[20]:
+
+
+del dic['a']
+
+
+# In[21]:
+
+
+print(dic)
+
+
+# In[34]:
+
+
+dic.values()
+
+
+# In[25]:
+
+
+dic.items()
+
+
+# In[26]:
+
+
+dic
+
+
+# In[30]:
+
+
+c=[1,2,3,3,3,4,41,3,1,3,5,101,11,1,1]
+d=set(c)
+d
+
+
+# In[35]:
+
+
+a=4
+print(a>=5)
+
+
+# if a>=5:
+#     print(a)
+
+# In[37]:
+
+
+while a==4:
+    a=a+1
+    print(a)
+
+
+# In[38]:
+
+
+while a<10:
+    a=a+1
+    print(a)
+
+
+# In[40]:
+
+
+a=[1,2,3,4]
+while a:
+    print(a)
+    a.pop()
+
+
+# In[44]:
+
+
+#mutable 객체, immutable 객체
+x=1
+y=2
+y=x
+x=y+1
+print(x)
+print(y)
+
+
+# In[ ]:
+
+
+x=[1,2,3]
+y=x
+
+
+# In[46]:
+
+
+money=True
+if money:
+    print("택시를 탄다")
+else:
+    print("걸어간다")
+
+
+# In[49]:
+
+
+money=False
+if money:
+    print("택시를 탄다")
+    print("택시를 타지 않는다")
+
+
+# In[52]:
+
+
+money=2000
+if money>=3000:
+    print("택시")
+else:
+    print("걷기")
+
+
+# In[55]:
+
+
+money=2000
+card=1
+if money>=3000 or card:
+    print("택시")
+else:
+    print("걷기")
+
+
+# In[56]:
+
+
+pocket=['paper','cellphone','money']
+if 'money'in pocket:
+    print("택시")
+else:
+    print("걷기")
+
+
+# In[57]:
+
+
+pocket=['paper','cellphone','money']
+if 'money'in pocket:
+     pass
+else:
+    print("걷기")
+
+
+# In[59]:
+
+
+pocket=['paper','cellphone','money']
+card=True 
+if 'money'in pocket:
+    print("택시")
+if card:
+    print("card")
+else:
+    print("걷기")
+
+
+# In[60]:
+
+
+pocket=['paper','cellphone','money']
+card=True 
+if 'money'in pocket:
+    print("택시")
+elif card:
+    print("card")
+else:
+    print("걷기")
+
+
+# In[62]:
+
+
+score=85
+if score>80:
+    print("A")
+elif score>70:
+    print("B")
+else:
+    print("F")
+
+
+# In[65]:
+
+
+#semantic error, logicat error.
+#자동으로는 에러를 못 고친다. 
+score=85
+if score>70:
+    print("B") #여기서 조건이 만족되어 버림
+elif score>80:
+    print("A") #따라서 여기서 부터의 조건은 무시됨. 
+else:
+    print("F")
+
+
+# In[66]:
+
+
+t=0
+while t<10:
+    print("나무를 %d번 찍습니다"%(t+1))
+    t=t+1
+
+
+# In[67]:
+
+
+t=0
+while t<10:
+    print("나무를 %d번 찍습니다"%(t+1))
+    t=t+1
+    if t==5:
+        break
+
+
+# In[68]:
+
+
+a=[1,2,3]
+for i in a:
+    print(i)
+
+
+# In[70]:
+
+
+a=[(1,2),(3,4),(5,6)]
+for (i,j) in a:
+    print(i+j)
+
+
+# In[3]:
+
+
+score=[65, 80, 75, 55, 90]
+n=0
+for i in score:
+    if i>=60:
+       print("pass")
+    else:
+       print("fail")
+    n=n+1
+
+
+# In[87]:
+
+
+for i in range(len(score)):
+    if score[i]>=60:
+        print("d%번 학생은 학격입니다."%i)
+    else:
+        print("d%번 학생은 불학격입니다."%i)
+
+
+# In[79]:
+
+
+sum=0
+#1부터 10까지의 합을 구하세요.
+#sum=0
+#sum=0+1, sum=1
+#i=2, sum=1+2
+#i=3 sum=sum+i sum=1+2+3
+#sum=sim+I
+for i in range(1,11):
+    sum=sum+i
+print(sum)
+
+
+# In[82]:
+
+
+#10부터 100까지 더해보세요.
+sum=0
+for i in range(1,101):
+    sum=sum+i
+print(sum)
+
+
+# In[83]:
+
+
+#구구단
+
+#8*9=72번 찍힌다.
+for i in range(2,10):
+    for j in range(1,10):
+        print("%dX%d=%d"%(i,j,i*j))
+
+
+# In[ ]:
+
+
+
+
